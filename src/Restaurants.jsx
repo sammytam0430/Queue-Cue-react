8701c00967bf3e8a07e1e3d806447dec752e17ab
Jen Yee <jenyee1022@yahoo.com>
2016-10-23 23:02:22 +0000

    not connecting to localhost

---
+++ src/Restaurants.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import data from './restaurants_data.js';
+
+const Restaurants = React.createClass({
+  render () {
+    console.log(data)
+  }
+})
--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,24 @@
 // Load up the application styles
 require("../styles/application.scss");
 
-// Render the top-level React component
+// React and Redux
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
+// Render the top-level React component
 import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+// create the store
+let store = createStore(reducers);
+let unsubscribe = store.subscribe(() =>
+  console.log("Index", store.getState())
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('react-root')
+);
