a2570cf2cb6eca9fbb9f0a5dfc7b350feb1fd056
Jen Yee <jenyee1022@yahoo.com>
2016-10-24 03:11:14 +0000

    added bootstrap styling

--- src/UsersContainer.jsx
+++ src/UsersContainer.jsx
@@ -1,60 +1,47 @@
 import React from 'react';
-// will receive the store state because it needs the existing users
-// will need to dispatch to the store to add people to the list
-
-// userState is the reducer that i need to write
-// this will pass down this.props.users to whatever component
-const UserContainer = React.createClass({
-  render () {
-    let name
-    let phone
-    return (
-      <div>
-        <form onSubmit={e => {
-          e.preventDefault();
-          this.props.addUser(name.value, phone.value)
-          name.value = ''
-          phone.value = ''
-        }}>
-          <input ref={node =>{
-            name = node
-          }} />
-          <input ref={node =>{
-            phone = node
-          }} />
-          <button type="submit">
-            Add User
-          </button>
-        </form>
-      </div>
-    )
-  }
-})
-
-const mapStateToProps = (store) => {
-  return {
-    users: store.addUser
-  }
-}
 
 var i = 0;
-const mapDispatchToProps = (dispatch) => {
+function addUser(name, phone) {
   return {
-    addUser: (name, phone) => {
-      dispatch({
-        type: 'ADD_USER',
-        id: i++,
-        name: name,
-        phone: phone
-      })
-    }
+    type: 'ADD_USER',
+    id: i++,
+    name: name,
+    phone: phone
   }
+
 }
 
-import { connect } from 'react-redux'
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(UserContainer);
+let UserContainer = ({ dispatch }) => {
+  let name
+  let phone
 
-// export default UserContainer;
+  return (
+    <div className="row">
+      <form className="form-inline" onSubmit={e => {
+        e.preventDefault();
+        dispatch(addUser(name.value, phone.value))
+        name.value = ''
+        phone.value = ''
+      }}>
+        <div className="form-group">
+          <input className="form-control" placeholder="Name" ref={node =>{
+            name = node
+          }} />
+        </div>
+        <div className="form-group">
+          <input type="email" className="form-control" placeholder="email" ref={node =>{
+            phone = node
+          }} />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Add User
+        </button>
+      </form>
+    </div>
+  )
+}
+
+
+
+import { connect } from 'react-redux';
+export default connect()(UserContainer);
