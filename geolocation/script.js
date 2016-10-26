05959879dcc87637fd3f8e2dc67c01ee511645b9
Sammy Tam <sammytam2005@hotmail.com>
2016-10-26 04:10:53 +0000

    getting geo location

--- geolocation/script.js
+++ geolocation/script.js
@@ -1,11 +1,16 @@
 window.onload = function() {
-  navigator.geolocation.getCurrentPosition((position) => {
-    var lat = position.coords.latitude;
-    var lon = position.coords.longitude;
-    showMap(lat, lon);
-  });
+  if ("geolocation" in navigator) {
+    /* geolocation is available */
+    navigator.geolocation.getCurrentPosition((position) => {
+      var lat = position.coords.latitude;
+      var lon = position.coords.longitude;
+      showMap(lat, lon);
+    });
+  } else {
+    alert('Your location is not available')
+  }
 }
 
 function showMap(lat, lon) {
-  console.log(lat + ',' + lon)
+  alert('Your location is: ' + lat + ', ' + lon)
 }
