d44a707582284772ef58f4bf4cb3c5332295703f
Sammy Tam <sammytam2005@hotmail.com>
2016-10-26 06:27:02 +0000

    append location to index

--- geolocation/script.js
+++ geolocation/script.js
@@ -1,16 +1,46 @@
 window.onload = function() {
-  if ("geolocation" in navigator) {
-    /* geolocation is available */
-    navigator.geolocation.getCurrentPosition((position) => {
-      var lat = position.coords.latitude;
-      var lon = position.coords.longitude;
-      showMap(lat, lon);
-    });
-  } else {
-    alert('Your location is not available')
-  }
-}
 
-function showMap(lat, lon) {
-  alert('Your location is: ' + lat + ', ' + lon)
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+  }
+  //Get the latitude and the longitude;
+  function successFunction(position) {
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    codeLatLng(lat, lng)
+  }
+
+  function errorFunction(){
+    alert("Geocoder failed");
+  }
+
+  function codeLatLng(lat, lng) {
+    let geocoder = new google.maps.Geocoder();
+    let latlng = new google.maps.LatLng(lat, lng);
+    geocoder.geocode({'latLng': latlng}, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        // console.log(results)
+        if (results[1]) {
+          for (var i=0; i<results[0].address_components.length; i++) {
+            for (var b=0;b<results[0].address_components[i].types.length;b++) {
+            //there are different types that might hold a city admin_area_lvl_1 usually does in come cases looking for sublocality type will be more appropriate
+              if (results[0].address_components[i].types[b] == "locality") {
+                //this is the object you are looking for
+                city= results[0].address_components[i];
+                break;
+              }
+            }
+          }
+          alert(city.long_name);
+          let main = document.getElementById("react-root");
+          let newContent = document.createTextNode('Your location is: ' + city.long_name);
+          document.body.insertBefore(newContent, main);
+        } else {
+          alert("No results found");
+        }
+      } else {
+        alert("Geocoder failed due to: " + status);
+      }
+    });
+  }
 }
