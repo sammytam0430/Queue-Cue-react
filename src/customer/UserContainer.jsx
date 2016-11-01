a9931c6c6093898ff9154464d54bac3b8ec87e31
to-y <yitong922@gmail.com>
2016-11-01 08:23:55 +0000

    styling of user page done except form && time && restaurant cards && centering of cards

--- src/customer/UserContainer.jsx
+++ src/customer/UserContainer.jsx
@@ -9,9 +9,7 @@ import { connect } from 'react-redux';
 import { reducers } from '../reducers.js';
 import RestaurantClient from '../rest_clients/restaurants.js';
 import RestaurantActions from '../actions.js';
-import SearchBar from '../general/SearchBar.jsx';
-import LocationBar from '../general/LocationBar.jsx';
-import UserReservation from '../general/UserReservation.jsx';
+
 function mapDispatchToProps(dispatch) {
   return {
     storeData() {
@@ -52,53 +50,28 @@ const UserContainer = React.createClass ({
     const { restaurants } = this.props;
     return (
     <div id="user-container">
-    <div id='map-cont'>
-      <div id='map'></div>
-    </div>
-       <div onClick={this.flipBox}>
-      ▼
-    </div>
-
-    <div className="table-container">
-            <nav id="user-nav">
-          <div className="title">
-            Queue
-          </div>
-            <span className="queuer queuer1">
-            <div className="triangle triangle1">
-            </div>
-            </span>
-            <span className="queuer queuer2">
-            <div className="triangle triangle2">
-            </div>
-            </span>
-            <span className="queuer queuer3">
-            <div className="triangle triangle3">
-            </div>
-            </span>
-          <div className="line line1" />
-            <SearchBar />
-            <LocationBar />
-          <div className="line line2" />
-          <div id="reserved-spot">
-            <UserReservation />
-          </div>
-        </nav>
-
-
-    <div className="toMap" onClick={this.flipBack}>
-      ▲
-    </div>
-        {this.props.restaurants.map((restaurant) => {
-          return (
-            <ListWidget
-            key={restaurant.id}
-            reservations={this.props.reservations}
-            restaurant={restaurant}
-            button1={AddReservationBtn}/>)
-        })}
+      <div id='map-cont'>
+        <div id='map'></div>
       </div>
+      <div className="toList" onClick={this.flipBox}>
+        ▼
       </div>
+      <div className="table-container">
+        <div className="toMap" onClick={this.flipBack}>
+          ▲
+        </div>
+        <div className="scroll">
+          {this.props.restaurants.map((restaurant) => {
+            return (
+              <ListWidget
+              key={restaurant.id}
+              reservations={this.props.reservations}
+              restaurant={restaurant}
+              button1={AddReservationBtn}/>)
+          })}
+        </div>
+      </div>
+    </div>
 
     );
   }
--- src/customer/UserMainLayout.jsx
+++ src/customer/UserMainLayout.jsx
@@ -9,6 +9,30 @@ const UserMainLayout = React.createClass ({
   render() {
     return (
       <div>
+        <nav id="user-nav">
+          <div className="title">
+            Queue
+          </div>
+          <span className="queuer queuer1">
+            <div className="triangle triangle1">
+            </div>
+          </span>
+          <span className="queuer queuer2">
+            <div className="triangle triangle2">
+            </div>
+          </span>
+          <span className="queuer queuer3">
+            <div className="triangle triangle3">
+            </div>
+          </span>
+          <div className="line line1" />
+            <SearchBar />
+            <LocationBar />
+          <div className="line line2" />
+          <div id="reserved-spot">
+            <UserReservation />
+          </div>
+        </nav>
         <div className="container">
           {this.props.children}
         </div>
